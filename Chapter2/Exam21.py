413080647040505878077847e3d82517f9033c35
Nguyen Thanh Thuy <thuynt.it@gmail.com>
2022-12-27 16:23:45 +0700

    modified:   Chapter2/Exam21.py 	new file:   Chapter2/Exam22.py

--- Chapter2/Exam21.py
+++ Chapter2/Exam21.py
@@ -1,21 +1,7 @@
 import requests
-import re
 
-url="https://phongtro123.com/tinh-thanh/da-nang"
-html = requests.get(url).text
+url = 'https://vnexpress.net/rss/khoa-hoc.rss'
 
+r = requests.get(url)
 
-TieuDe=re.findall('<h3 class="post-title"><a.*?">(.*?)</a></h3>', html)
-DonGia=re.findall('<span class="post-price">(.*?)</span>', html)
-DienTich=re.findall('<span class="post-acreage">(.*?)</span>', html)
-DiaChi=re.findall('<span class="post-location"><a.*?">(.*?)</a></span>', html)
-NgayDang=re.findall('<time class="post-time" title=.*?">(.*?)</time>', html)
-
-for i in range(len(TieuDe)):
-    print(TieuDe[i])
-    print(" - " + DonGia[i])
-    print(" - " + DienTich[i])
-    print(" - " + DiaChi[i])
-    print(" - " + NgayDang[i])
-
-
+print(r.text)
