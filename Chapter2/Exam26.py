a6a6d41b72f040e403b9b9e4f8af06d2abf3f1eb
Nguyen Thanh Thuy <thuynt.it@gmail.com>
2022-12-29 18:41:19 +0700

    new file:   Chapter2/Exam26.py

---
+++ Chapter2/Exam26.py
@@ -0,0 +1,61 @@
+import requests
+import re
+import os
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin, urlsplit
+
+links_seen=[]
+links_todo = ["https://phongtro123.com/"]
+domain= urlsplit(links_todo[0]).netloc
+file_store="E:/Files_Crawl/"
+
+def url_to_file_name(url):
+    url = str(url).strip().replace(' ', '_')
+    return re.sub(r'(?u)[^-\w.]', '', url) + ".html"
+
+def download(url):
+    print("Saving file,...")
+    filename=os.path.join(file_store, url_to_file_name(url))    
+    with open(filename, 'w',encoding='utf-8') as the_html:
+            the_html.write(requests.get(url).text)
+    
+def visit(url):
+    global links_todo
+    
+    links_seen.append(url)
+    print("** Now visiting:", url)
+    
+    html = requests.get(url).text
+    html_soup = BeautifulSoup(html, "html.parser")
+    download(url)
+    
+    Count=0
+    for link in html_soup.find_all("a"):
+        link_url = link.get("href")  
+        if link_url is None:
+            continue
+        
+        full_url = urljoin(url, link_url)
+        
+        if urlsplit(full_url).netloc != domain:
+            continue
+
+        if urlsplit(full_url).query !="":
+            continue
+        
+        if full_url in links_todo or full_url in links_seen:
+            continue
+        
+        Count=Count+1
+        links_todo.append(full_url)
+
+    print(" + ",Count, "new link(s) found")
+    print(" + ",len(links_seen), "link(s) seen/", len(links_todo), "link(s) need to do.")
+    
+    print("Enter to continues, ...")
+    input()
+
+
+while links_todo is not None:
+    url_to_visit = links_todo.pop()
+    new_links = visit(url_to_visit)
